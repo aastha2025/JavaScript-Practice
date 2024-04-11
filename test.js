bc20845dfd8d53a012f95de4719c0046f6875c74
aastha2025 <aastha2000sh@gmail.com>
2024-04-11 20:16:36 +0530

    DONE

--- test.js
+++ test.js
@@ -13,3 +13,11 @@ console.log(accountId);
 console.table([accountId , accountEmail,accountPassward,accountCity, accountState])
 
 console.log(typeof null);
+
+/*
+Always prefer let 
+Don't use var because it has some functional and block scope issues.
+*/
+
+
+//error cannot redeclare the same variable again with let
