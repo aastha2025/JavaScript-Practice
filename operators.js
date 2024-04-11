086863e22611c13820aaf3f6b014f7c850ac06a3
aastha2025 <aastha2000sh@gmail.com>
2024-04-11 21:10:35 +0530

    DONE

---
+++ operators.js
@@ -0,0 +1,61 @@
+//<-----------------OPERATORS--------------->
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+
+
+
+// console.log(2 + 3 + "4");
+// console.log("4" + 3 + 3);
+// console.log("4" + 8);
+// console.log(2 + "4");
+// console.log(null + "4");
+// console.log("4" + undefined);
+// console.log(NaN + "4");
+// console.log("4" + false);
+
+// string se phle conversion hoga but string ke baad string hi consider hoga
+/*
+console.log(true);
+console.log(+true);
+console.log(true+);
+console.log(+"");
+let gameCounter =100
+++gameCounter;
+gameCounter++;
+console.log(gameCounter);
+
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2==1);
+console.log(2!=1);
+
+console.log("2">1);
+console.log("02">1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+console.log(undefined == 0);
+console.log(undefined >=0);
+console.log( undefined<0);
+
+console.log("2"===2); // = true , == false , === true 
+
+*/
+
+// javascript me comparision operators datatype ko bhi change kr dete hai 
+// so hum javascript me kbhi kbhi strict check krte hai...
+
+// for strict check we use ===
+// for check/comparision ==
+
+// == it can convert datatype too
+
+console.log("2" == 2);
+console.log("2" === 2);
+
+// all operator are used as other languages
