80c8a09b4786bf86a3bb3e07be0c4cb3b7131581
aastha2025 <aastha2000sh@gmail.com>
2024-04-11 20:58:24 +0530

    DONE

---
+++ conversion operators.js
@@ -0,0 +1,43 @@
+"use strict" // treat all code of javaScript in newer version
+
+//  alert(3+3); // code readability should be high
+// we are using NodeJs not Browser
+
+ //<-------------DATA TYPES ------------>
+ /*  number = 2 to power 52
+      bigint 
+      string =  ""
+      boolean = true/false
+       null = standalone value (type = object)
+ undefined = kindda place holder for a value which is not defined yet (type = undefined)
+ symbol = uniquness
+
+ NULL and UNDEFINED both are different
+
+ # "use strict";   treat all code as newer version of JS
+
+*/
+
+let score = undefined
+
+console.log(typeof score);
+console.log(typeof (score)); //method form declaration
+
+let valueofno = Number(score)
+console.log(typeof valueofno);
+console.log(valueofno);
+
+let islogedin = 1;
+let boolislogedin = Boolean(islogedin)
+console.log(boolislogedin);
+
+
+
+
+
+
+
+
+
+
+
