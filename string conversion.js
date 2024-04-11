f2831cce75ac1ac6f8de18a4247c97c41b9baed9
aastha2025 <aastha2000sh@gmail.com>
2024-04-11 21:04:42 +0530

    DONE

---
+++ string conversion.js
@@ -0,0 +1,48 @@
+//<---------------STRING CONVERSION------------------>
+let someno = 33
+let stringno = String(someno)
+console.log(stringno);
+console.log(typeof stringno);
+
+ /* one more example*/
+
+// console.log("Example wih null:");
+//  const a = null;
+//  let b = Number(a);
+//  console.log(typeof(b));
+//  console.log(b);
+
+
+ /* one more example*/
+ 
+// console.log("Example wih undefined:");
+//  const c = undefined;
+//  let d = Number(c);
+//  console.log(typeof(d));
+//  console.log(d);
+
+ /*
+
+  /*
+ Notes
+            conversion to integer
+            "33" => 33
+            "33abc" => NaN(Not a Number)
+            true => 1, false => 0
+            null => NaN
+            undefined => NaN
+
+            Conversion to boolean
+            1 => true 0 => false
+            "" => false
+            "Aryan" => true
+
+            conversion to string
+            33 => "33"
+ */
+
+// console.log("From boolean to string: =>");
+// const u = 0;
+// let f = String(u);
+// console.log(f);
+
