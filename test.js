80dd0368481b2849c086abc237dd684fafce53ec
aastha2025 <aastha2000sh@gmail.com>
2024-04-11 20:14:23 +0530

    DONE

---
+++ test.js
@@ -0,0 +1,15 @@
+const accountId = 12345
+let accountEmail = "aastha2000@gmail.com"
+var accountPassward = "12349"
+accountCity="goa"
+let accountState;
+
+accountEmail="ac@gmail.com"
+accountPassward="12321"
+accountCity="bengaluru"
+
+console.log(accountId);
+
+console.table([accountId , accountEmail,accountPassward,accountCity, accountState])
+
+console.log(typeof null);
